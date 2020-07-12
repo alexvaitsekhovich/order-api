632601f0e6fc03afbdb235709d1afb4a97c20b97
alexvaitsekhovich <alex.vaitsekhovich@gmail.com>
2020-07-12 16:34:42 +0200

    Removed unused variables

--- src/test/java/com/alexvait/orderapi/mapper/OrderMapperTest.java
+++ src/test/java/com/alexvait/orderapi/mapper/OrderMapperTest.java
@@ -12,11 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @DisplayName("Test Order-DTO mapper")
 class OrderMapperTest {
 
-    private static final String CITY = "Berlin";
-    private static final String ZIP = "10115";
-    private static final String STREET = "Chausseestr.";
-    private static final String NR = "1";
-
     private final OrderMapper orderMapper = OrderMapper.INSTANCE;
 
     @Test
