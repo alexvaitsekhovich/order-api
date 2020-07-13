9be3b8e58f38328dc3ec979aa2ca48147d975c62
alexvaitsekhovich <alex.vaitsekhovich@gmail.com>
2020-07-13 23:29:05 +0200

    Swagger integration

--- src/main/java/com/alexvait/orderapi/dto/OrderDto.java
+++ src/main/java/com/alexvait/orderapi/dto/OrderDto.java
@@ -1,9 +1,9 @@
 package com.alexvait.orderapi.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +13,22 @@ import java.util.List;
 public class OrderDto {
     private Long id;
 
-    @NotBlank
     private String number;
 
     private Integer statusId;
 
     @NotNull(message = "Payment id is mandatory")
+    @ApiModelProperty(required = true)
     private Integer paymentId;
 
     @NotNull(message = "Amount is mandatory")
+    @ApiModelProperty(value = "Order amount in smallest currency, as integer", required = true)
     private Integer amount;
 
     private Integer discountAmount;
 
     @NotNull(message = "Address is mandatory")
+    @ApiModelProperty(required = true)
     private AddressDto address;
 
     @NotNull(message = "An array of order parts is mandatory")
