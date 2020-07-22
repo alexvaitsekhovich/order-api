b4ae09e617bfd9143cfe35810a08d1934a1e7c9b
alexvaitsekhovich <alex.vaitsekhovich@gmail.com>
2020-07-22 23:40:15 +0200

    Added tests for time mapper

--- src/test/java/com/alexvait/orderapi/mapper/OrderMapperTest.java
+++ src/test/java/com/alexvait/orderapi/mapper/OrderMapperTest.java
@@ -5,6 +5,9 @@ import com.alexvait.orderapi.entity.Order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+
 import static com.alexvait.orderapi.testobjects.TestOrder.testOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -56,4 +59,30 @@ class OrderMapperTest {
         assertEquals(0, orderDto.getOrderParts().size());
     }
 
+    @Test
+    void timestampToOffsetDateTime() {
+        Timestamp ts = Timestamp.valueOf("2020-03-12 13:11:22.0");
+        OffsetDateTime odt = orderMapper.timestampToOffsetDateTime(ts);
+
+        assertEquals(2020, odt.getYear());
+        assertEquals(3, odt.getMonthValue());
+        assertEquals(12, odt.getDayOfMonth());
+        assertEquals(13, odt.getHour());
+        assertEquals(11, odt.getMinute());
+        assertEquals(22, odt.getSecond());
+    }
+
+    @Test
+    void offsetDateTimeToTimestamp() {
+        OffsetDateTime odt = OffsetDateTime.parse("2020-03-12T13:11:22+00:00");
+        Timestamp ts = orderMapper.offsetDateTimeToTimestamp(odt);
+
+        assertEquals(2020, ts.toLocalDateTime().getYear());
+        assertEquals(3, ts.toLocalDateTime().getMonthValue());
+        assertEquals(12, ts.toLocalDateTime().getDayOfMonth());
+        assertEquals(13, ts.toLocalDateTime().getHour());
+        assertEquals(11, ts.toLocalDateTime().getMinute());
+        assertEquals(22, ts.toLocalDateTime().getSecond());
+
+    }
 }
